1cca54250888c89bf32c5053130292559f61c4d7
manuelRestrep0 <94997816+manuelRestrep0@users.noreply.github.com>
2022-07-31 17:24:30 -0500

    summit 1

---
+++ taller2Logica3/src/main/java/com/mycompany/taller2logica3/main.java
@@ -0,0 +1,9 @@
+package com.mycompany.taller2logica3;
+
+public class main {
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
---
+++ taller2Logica3/src/main/java/com/mycompany/taller2logica3/raiz.java
@@ -0,0 +1,21 @@
+package com.mycompany.taller2logica3;
+
+public class raiz {
+    String valor;
+    raiz[] hijos;
+    raiz padre;
+    raiz liga; //el primer hijo.
+    int sw;
+    int nivel;
+    int grado;
+    
+    public raiz(String valor, raiz[] hijos) {
+        this.valor = valor;
+        this.hijos = hijos;
+    }
+    
+    
+
+    
+    
+}
